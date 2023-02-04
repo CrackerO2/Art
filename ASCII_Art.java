9d6a09972eba152be4f05d444dcdba5658ca9ea6
Cracker02 <sbajracharya02@gmail.com>
2023-02-04 23:13:15 +0100

    Fixed image Orientation

--- ASCII_Art.java
+++ ASCII_Art.java
@@ -9,40 +9,31 @@ public class ASCII_Art
 
     public static void main(String[] args)
     {
-        File image = new File("/Users/shrabin/Desktop/Project_Files/Java/Art/chess16-2.gif");
+        File image = new File("/Users/shrabin/Desktop/Project_Files/Java/Art/mario_jump.jpeg"); //add path of the file
         int height = 0;
         int width = 0;
 
         Date x = new Date();
 
-        
-        
-
         try {
             
             height = ImageIO.read(image).getTileHeight();
-            width = ImageIO.read(image).getTileWidth();
-            
-            
+            width = ImageIO.read(image).getTileWidth(); 
         } catch (IOException e) {
         }
-
 
         System.out.println(x.getTime());
         for (int i = 0; i < height; i++)
         {
             for (int j = 0; j < width; j++)
             {
-                try{
-                    
-                if(ImageIO.read(image).getRGB(i, j) == -1)
-                {
-                    System.out.print("00");
-                }
-                else
-                {
-                    System.out.print("  ");
-                }
+                try{       
+                    if(ImageIO.read(image).getRGB(j, i) == -1){
+                        System.out.print("00");
+                    }
+                    else{
+                        System.out.print("  ");
+                    }
                 }
                 catch (IOException e)
                 {}
